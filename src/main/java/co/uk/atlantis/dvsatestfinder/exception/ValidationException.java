cdc66fe6b045523a36a0907082528d47cf3a28e6
eadogan <e.ahmetdogan@gmail.com>
2020-09-06 01:26:22 +0100

    ASIS-01: Created Exception Classes

---
+++ src/main/java/co/uk/atlantis/dvsatestfinder/exception/ValidationException.java
@@ -0,0 +1,18 @@
+package co.uk.atlantis.dvsatestfinder.exception;
+//
+//import java.util.Collections;
+//import java.util.List;
+//
+//public class ValidationException extends RuntimeException {
+//
+//    private final List<ErrorDto> errors;
+//
+//    public ValidationException(List<ErrorDto> errors) {
+//        super("Invalid inputs");
+//        this.errors = Collections.unmodifiableList(errors);
+//    }
+//
+//    public List<ErrorDto> getErrors() {
+//        return Collections.unmodifiableList(this.errors);
+//    }
+//}
