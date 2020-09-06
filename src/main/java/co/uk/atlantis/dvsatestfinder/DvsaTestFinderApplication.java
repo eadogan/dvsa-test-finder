8bc5da6d1917a425ad719fa5d8a8bde5da2350ef
eadogan <e.ahmetdogan@gmail.com>
2020-09-06 01:02:09 +0100

    created dvsa-test-finder

---
+++ src/main/java/co/uk/atlantis/dvsatestfinder/DvsaTestFinderApplication.java
@@ -0,0 +1,13 @@
+package co.uk.atlantis.dvsatestfinder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DvsaTestFinderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DvsaTestFinderApplication.class, args);
+	}
+
+}
