59c99c8f9bd9f943a5a1205d3b13c98ceead0493
eadogan <e.ahmetdogan@gmail.com>
2020-09-06 12:50:54 +0100

    ASIS-03: Refactor Scheduler Class

--- src/main/java/co/uk/atlantis/dvsatestfinder/scheduler/TestFinderScheduler.java
+++ src/main/java/co/uk/atlantis/dvsatestfinder/scheduler/TestFinderScheduler.java
@@ -1,4 +1,26 @@
 package co.uk.atlantis.dvsatestfinder.scheduler;
 
+import co.uk.atlantis.dvsatestfinder.service.FindAvailableTestService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@Component
 public class TestFinderScheduler {
+
+    private FindAvailableTestService findAvailableTestService;
+
+    public TestFinderScheduler(FindAvailableTestService findAvailableTestService) {
+        this.findAvailableTestService = findAvailableTestService;
+    }
+
+    // every 5 mins run
+    @Scheduled(cron = "${cron.available-date.checker}")
+    public void runChecker(){
+        log.info("Cron Job: Available Test Service Started");
+        System.out.println(findAvailableTestService.findAvailableTestDate());
+        log.info("Cron Job: Available Test Service Ended");
+    }
+
 }
