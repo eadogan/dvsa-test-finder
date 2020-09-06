214c0eac5ff15bf3ed1af37947569cdeaf1c9c30
eadogan <e.ahmetdogan@gmail.com>
2020-09-06 13:42:54 +0100

    ASIS-03: Refactor Application file

--- src/main/java/co/uk/atlantis/dvsatestfinder/DvsaTestFinderApplication.java
+++ src/main/java/co/uk/atlantis/dvsatestfinder/DvsaTestFinderApplication.java
@@ -2,8 +2,10 @@ package co.uk.atlantis.dvsatestfinder;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class DvsaTestFinderApplication {
 
 	public static void main(String[] args) {
